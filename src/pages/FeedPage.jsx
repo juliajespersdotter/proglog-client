58e307790bb28a94caba43088487bdcf4cf54e0d
Julia Jespersdotter <julia.jespersdotter@gmail.com>
2023-01-02 14:55:15 +0100

    button pngs and sidebars styling.

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 
 const FeedPage = () => {
-	return <h1>FeedPage</h1>
+	return (
+		<div id='container' className='feedpage'>
+			<div className='sidebar'>
+				<button className='button button--primary'>PROFILE</button>
+				<button className='button button--primary'>LIBRARY</button>
+				<button className='button button--primary'>NEWS</button>
+				<button className='button button--primary'>BROWSE</button>
+			</div>
+			<div className='feed'>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Voluptates fugiat dolore numquam unde facere sunt eum
+					nesciunt laborum? Corporis libero at earum nihil numquam
+					illum alias dolorem suscipit officiis natus.
+				</p>
+			</div>
+			<div className='sidebar'></div>
+		</div>
+	)
 }
 
 export default FeedPage
