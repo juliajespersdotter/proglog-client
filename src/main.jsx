8412eba99968cc45c0ca7c9684b43d70707d6330
Julia Jespersdotter <julia.jespersdotter@gmail.com>
2023-01-02 13:16:17 +0100

    sass globals and variables and installed required libraries.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
+// import AuthContextProvider from './contexts/AuthContext'
 import App from './App'
-import './index.css'
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			staleTime: 1000 * 60 * 2, // 2 minutes
+			cacheTime: 1000 * 60 * 60 * 4, // 4 hours
+		},
+	},
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<BrowserRouter>
+			<QueryClientProvider client={queryClient}>
+				{/* <AuthContextProvider> */}
+				<App />
+				{/* </AuthContextProvider> */}
+			</QueryClientProvider>
+		</BrowserRouter>
+	</React.StrictMode>
 )
---
+++ src/pages/FeedPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const FeedPage = () => {
+	return <h1>FeedPage</h1>
+}
+
+export default FeedPage
