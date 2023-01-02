5141fff309e01a17e9ddee3e5197dcc68e3265c5
Julia Jespersdotter <julia.jespersdotter@gmail.com>
2023-01-02 14:22:45 +0100

    further navigation styling.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import FeedPage from './pages/FeedPage'
 import NotFound from './components/NotFound'
+import Navigation from './components/Navigation'
 import './assets/styles/globals.scss'
 
 function App() {
 	return (
 		<div id='App'>
 			{/**Navigation**/}
+			<Navigation />
 			{/**Routes**/}
 			<Routes>
 				<Route path='/' element={<FeedPage />} />
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BiMessageRounded, BiBell, BiCog } from 'react-icons/bi'
+import { FiUsers } from 'react-icons/fi'
+
+const Navigation = () => {
+	return (
+		<nav id='navigation'>
+			<h4>WELCOME, PLAYER</h4>
+			<form action='submit'>
+				<input type='text' className='inputbox' placeholder='SEARCH_' />
+			</form>
+			<ul id='nav-links'>
+				<li>
+					<FiUsers />
+				</li>
+				<li>
+					<BiMessageRounded />
+				</li>
+				<li>
+					<BiBell />
+				</li>
+				<li>
+					<BiCog />
+				</li>
+			</ul>
+		</nav>
+	)
+}
+
+export default Navigation
