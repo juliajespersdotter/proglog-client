2d4a1f6b93cdc2ce59d6441be80017f286457f61
Julia Jespersdotter <julia.jespersdotter@gmail.com>
2023-01-21 12:56:55 +0100

    removed proxy.

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		proxy: {
-			//server proxy
-			'/api': import.meta.env.VITE_API_BASE_URL,
-		},
-	},
 })
